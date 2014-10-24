5c3c2405344e43369f18692776484c28dd979206
nitor <brett.mulligan@gmail.com>
2014-10-24 17:29:01 -0500

    Getting there.

--- maketest.py
+++ maketest.py
@@ -1,8 +1,27 @@
 import random
+import re
+
+mqffn = 'mqf'
 
 FIRST_QUESTION = 1
 TOTAL_QUESTIONS = 106
 TEST_QUESTIONS = 25
+
+questions = {}
+
+def read_file():
+    data = []
+    f = open(mqffn, 'r+')
+
+    if not f:
+        print 'error opening file', mqffn
+    else:
+
+        for line in f:
+            data.append(line)
+
+    return data
+
 
 qs = []
 
@@ -18,9 +37,34 @@ qs.sort()
 
 print qs
 
+data = read_file()
 
-def get_q(q):
-    for x in q:
-        yield q.pop(0)
+for line in data:
+    print line
 
-# print get_q(qs)
+
+q_start = re.compile('(\d?\d?\d\.)')
+
+split_data = q_start.split(''.join(data))
+
+print 'SPLIT:'
+for x in range(len(split_data)):
+    print 'INDEX:', x, 'TEXT:', split_data[x]
+
+    # split_data[x] = split_data[x].strip()
+
+    if q_start.match(split_data[x]):
+        questions[int(split_data[x].strip('.'))] = split_data[x + 1]
+
+keys = questions.keys()
+keys.sort()
+
+print 'BY KEY:'
+for key in keys:
+    print key, questions[key]
+
+print 'KEYS:', len(questions.keys())
+
+print split_data
+print 'Length:', len(split_data)
+print 'END'
